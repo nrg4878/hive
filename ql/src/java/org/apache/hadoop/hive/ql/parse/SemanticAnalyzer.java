6df5639790247b8bb30e020407f3d5359554fbe3
Naveen Gangam <ngangam@cloudera.com>
2020-11-30 01:13:01 -0500

    HIVE-24396 Moving create/drop/alter APIs to the interface. Reverting fix for case sensitivity

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -1116,7 +1116,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
 
     ASTNode tableTree = (ASTNode) (tabref.getChild(0));
 
-    String tabIdName = getUnescapedName(tableTree);
+    String tabIdName = getUnescapedName(tableTree).toLowerCase();
 
     String alias = findSimpleTableName(tabref, aliasIndex);
 
